4021e0ef2aa4aeb53e992175127e52251d1b5ff6
Nathan Genetzky <nathan@genetzky.us>
2019-10-15 01:05:03 -0500

    hooks: Install initial virtualenvwrapper hooks with stub impl

--- setup.py
+++ setup.py
@@ -46,10 +46,23 @@ setup(
     ],
     # namespace_packages=['virtualenvwrapper'], # TODO
     packages=find_packages(),
-    # entry_points={ # TODO
-    #     'virtualenvwrapper.project.template': [
-    #         'vew = virtualenvwrapper.vew:template',
-    #     ],
-    # },
-    # include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'vew = vew.__main__:main',
+        ],
+        'virtualenvwrapper.get_env_details': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.initialize': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.postactivate': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.postdeactivate': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.postmkproject': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.postmkvirtualenv': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.postrmvirtualenv': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.preactivate': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.predeactivate': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.premkproject': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.premkvirtualenv': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.prermvirtualenv': ['vew = vew.__main__:stub'],
+        'virtualenvwrapper.pre_activate': ['vew = vew.__main__:stub'],
+    },
+    include_package_data=True,
 )
--- vew/__main__.py
+++ vew/__main__.py
@@ -5,11 +5,14 @@ from vew.util import logging_setup, PACKAGE_NAME
 
 LOG = logging.getLogger(PACKAGE_NAME)
 
+def stub(argv=sys.argv[1:]):
+    logging_setup()
+    LOG.info(argv)
 
-def main(argv=sys.argv[1:]):
+def main(argv=sys.argv[0:]):
     logging_setup()
     LOG.info(argv)
 
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv[0:]))
